b7ddc5e793bbba54e97d1fd32194bf2383ffce34
zemosamurai <zemosamurai@gmail.com>
2022-10-29 17:01:37 +0300

    add addTask and add  v1()

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
 import TodoList from "./TodoList";
+import {v1} from "uuid";
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
@@ -16,29 +17,44 @@ function App() {
     //BLL:
     const TodoListTitle: string = "What to learn"
     const [tasksForTodoList, setTasksForTodoList] = useState<Array<TaskType>>([
-            {id: 1, title: "HTML&CSS", isDone: true},
-            {id: 2, title: "JS & ES6", isDone: true},
-            {id: 3, title: "React", isDone: false},
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS & ES6", isDone: true},
+            {id: v1(), title: "React", isDone: false},
         ]
     )
 
-
     const [filter, setFilter] = useState<FilterValuesType>('all')
-    let filteredTasks = tasksForTodoList
 
-    if (filter === 'active') {
-        filteredTasks = tasksForTodoList.filter(t => t.isDone === false)
+    const getFilteredTasks = (tasks: Array<TaskType>, filterValue: FilterValuesType) => {
+        let filteredTasks = tasks
+
+        if (filterValue === 'active') {
+            filteredTasks = tasks.filter(t => !t.isDone)
+        }
+        if (filterValue === 'completed') {
+            filteredTasks = tasks.filter(t => t.isDone)
+        }
+        return filteredTasks
     }
-    if (filter === 'completed') {
-        filteredTasks = tasksForTodoList.filter(t => t.isDone === true)
-    }
+
+    const filteredTasks = getFilteredTasks(tasksForTodoList, filter)
+
 
     const changeFilter = (filter: FilterValuesType) => {
         setFilter(filter)
     }
 
-    const removeTask = (taskId: number) => {
+    const removeTask = (taskId: string) => {
         setTasksForTodoList(tasksForTodoList.filter(task => task.id !== taskId))
+    }
+
+    const addTask = (title: string) => {
+        const newTask: TaskType = {
+            id: v1(), // пакет который генирирует id v1()
+            title,           // title: title
+            isDone: false
+        }
+        setTasksForTodoList([newTask, ...tasksForTodoList])
     }
 
 
@@ -48,6 +64,7 @@ function App() {
             <TodoList
                 title={TodoListTitle}
                 tasks={filteredTasks}
+                addTask={addTask}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
             />
