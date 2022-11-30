f0a2afb0bf87933a3aaeeaa0b80dab03ee235ddb
zemosamurai <zemosamurai@gmail.com>
2022-11-30 12:32:32 +0300

    export type

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,12 @@ export type TaskType = {
     isDone: boolean
 }
 export type FilterValuesType = 'all' | 'active' | 'completed'
-type TodolistType = {
+export type TodolistType = {
     id: string
     title: string
     filter: FilterValuesType
 }
-type TasksStateType = {
+export type TasksStateType = {
     [todoListId: string]: Array<TaskType>
 }
 
