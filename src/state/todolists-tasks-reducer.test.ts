e19ae2a26183f7e8e04e77a9a8c5f322190a9a6b
zemosamurai <zemosamurai@gmail.com>
2022-12-17 13:48:42 +0300

    refactor test

--- src/state/todolists-tasks-reducer.test.ts
+++ src/state/todolists-tasks-reducer.test.ts
@@ -1,20 +1,20 @@
 import {TasksStateType, TodolistType} from "../App";
-import {addTodoListAC, TodolistReducer} from "./todolist-reducer";
-import {TasksReducer} from "./tasks-reducer";
+import {addTodoListAC, todolistReducer} from "./todolist-reducer";
+import {tasksReducer} from "./tasks-reducer";
 
 test('ids should be equals', () => {
     const startTasksState: TasksStateType = {}
-    const startTodolistsState: Array<TodolistType> = []
+    const startTodoListsState: Array<TodolistType> = []
 
     const action = addTodoListAC('new todolist')
 
-    const endTasksState = TasksReducer(startTasksState, action)
-    const endTodolistsState = TodolistReducer(startTodolistsState, action)
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodoListsState = todolistReducer(startTodoListsState, action)
 
     const keys = Object.keys(endTasksState)
     const idFromTasks = keys[0]
-    const idFromTodolists = endTodolistsState[0].id
+    const idFromTodoLists = endTodoListsState[0].id
 
     expect(idFromTasks).toBe(action.payload.todoId)
-    expect(idFromTodolists).toBe(action.payload.todoId)
+    expect(idFromTodoLists).toBe(action.payload.todoId)
 })
