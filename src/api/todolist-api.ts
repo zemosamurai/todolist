229ad7257d220dbad6d443175a971e03c831ea89
zemosamurai <zemosamurai@gmail.com>
2023-01-11 11:12:51 +0300

    add new type and create todolistAPI

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,17 +1,59 @@
-import axios from "axios";
-
-type TodolistType = {
-    id: string
-    title: string
-    addedDate: string
-    order: number
-}
+import axios from "axios"
 
 type ResponseType<T = {}> = {
     data: T
     fieldsErrors: string[]
     messages: string[]
     resultCode: number
+}
+export type TodolistType = {
+    id: string
+    title: string
+    addedDate: string
+    order: number
+}
+
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    Hi = 2,
+    Urgently = 3,
+    Later = 4
+}
+
+export type TaskType = {
+    description: string
+    title: string
+    completed: boolean
+    status: TaskStatuses
+    priority: number
+    startDate: string
+    deadline: string
+    id: string
+    todoListId: string
+    order: number
+    addedDate: string
+}
+type GetTaskType = {
+    items: TaskType[]
+    totalCount: number
+    error: string
+}
+type UpdateTaskModelType = {
+    title: string
+    description: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
 }
 
 const instance = axios.create({
@@ -39,35 +81,6 @@ export const todolistAPI = {
         return instance.put<ResponseType>(`/todo-lists/${todolistId}`, {title})
             .then(res => res.data)
     },
-}
-
-
-type TaskType = {
-    description: string
-    title: string
-    completed: boolean
-    status: number
-    priority: number
-    startDate: string
-    deadline: string
-    id: string
-    todoListId: string
-    order: number
-    addedDate: string
-}
-type GetTaskType = {
-    items: TaskType[]
-    totalCount: number
-    error: string
-}
-type UpdateTaskModelType = {
-    title: string
-    description: string
-    completed: boolean
-    status: number
-    priority: number
-    startDate: string
-    deadline: string
 }
 
 export const taskAPI = {
